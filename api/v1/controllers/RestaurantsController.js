a071d92fe975fd2aa73fee90fba983ec370306bd
Charles DESGENETEZ <dorpaxione@gmail.com>
2021-05-17 18:26:41 +0200

    populate on restaurants

--- api/v1/controllers/RestaurantsController.js
+++ api/v1/controllers/RestaurantsController.js
@@ -1,11 +1,24 @@
 const Restaurant = require('../models/users/Restaurant');
-const {RestaurantNotFound} = require('../errors');
+const {RestaurantNotFoundError} = require('../errors');
 
 const excludedFields = ['first_name', 'last_name', 'email', 'phone', 'products', 'admin', 'type'];
+const population = {
+    path: 'menus',
+    populate: {
+        path: 'products',
+        populate: {
+            path: 'product',
+            populate: {
+                path: 'extras',
+                populate: 'product'
+            }
+        }
+    }
+}
 
 exports.getRestaurants = function (req, res, next) {
     const select = excludedFields.reduce((obj, next) => obj = {...obj, [next]: 0}, {});
-    const query = Restaurant.find({}).select(!req.user.admin ? select : {});
+    const query = Restaurant.find({}).populate(population).select(!req.user.admin ? select : {});
     return query.exec().then(function (restaurants) {
         return res.status(200).json(restaurants);
     }).catch(next);
@@ -14,9 +27,9 @@ exports.getRestaurants = function (req, res, next) {
 exports.restaurantParamMiddleware = function (req, res, next, restaurantId) {
     const canSeeAllFields = req.user.admin || req.user._id.equals(restaurantId);
     const select = excludedFields.reduce((obj, next) => obj = {...obj, [next]: 0}, {});
-    const query = Restaurant.findOne({_id: restaurantId}).select(!canSeeAllFields ? select : {});
+    const query = Restaurant.findOne({_id: restaurantId}).populate(population).select(!canSeeAllFields ? select : {});
     return query.exec().then(function(restaurant) {
-        if(!restaurant) throw new RestaurantNotFound();
+        if(!restaurant) throw new RestaurantNotFoundError();
         req.restaurant = restaurant;
         return next();
     }).catch(next);
