922185eaf9320a8b2712619e997b85c9b214fce3
C.Thomas <c.tom34270@gmail.com>
2021-05-21 17:23:12 +0200

    Add orders data

--- scripts/fillDatabase.sh
+++ scripts/fillDatabase.sh
@@ -2,3 +2,4 @@ mongoimport --db apeat --collection users --file ./jsons/users.json --jsonArray 
 mongoimport --db apeat --collection products --file ./jsons/products.json --jsonArray --drop
 mongoimport --db apeat --collection menus --file ./jsons/menus.json --jsonArray --drop
 mongoimport --db apeat --collection carts --file ./jsons/carts.json --jsonArray --drop
+mongoimport --db apeat --collection orders --file ./jsons/orders.json --jsonArray --drop
